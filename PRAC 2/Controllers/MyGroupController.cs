2878a65ac1afc90a5707c46c7830f18ab5b111d0
Priscilla Nembelwe <Priscilla Nembelwe@PRISCILLA>
2025-03-10 23:03:29 +0200

    Add project files.

---
+++ PRAC 2/Controllers/MyGroupController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PRAC_2.Models; 
+
+namespace PRAC_2.Controllers
+{
+    public class MyGroupController : Controller
+    {
+        // GET: MyGroup
+        public ActionResult Index()
+        {
+            var members = new List<MyGroup>
+            {
+                new MyGroup { studentNumber = "u23755629", firstName = "Pricilia", lastName = "Nembelwe", emailAddress = "pricilia.nembelwe@gmail.com"  },
+                new MyGroup { studentNumber = "u36356483", firstName = "Sharon", lastName = "Nkanyani", emailAddress = "sharon.nkanyani@gmail.com" },
+                new MyGroup { studentNumber = "u98369836", firstName = "Tshilisanani", lastName = "Mamathuntsha", emailAddress = "tshilisanani.mamathuntsha@gmail.com"},
+                new MyGroup { studentNumber = "u35168187", firstName = "Lugisani", lastName = "Mukhwathi", emailAddress = "lugisani.mukhwathi@gmail.com"},
+                new MyGroup { studentNumber = "u73466466", firstName = "Collin", lastName = "Nembilwi", emailAddress = "collin.nembilwi@gmail.com"}
+            };
+            return View(members);
+
+            
+        }
+
+        public ActionResult PersonalPage(string id)
+        {
+            return View(id);  // Will look for a view named "john.cshtml", "jane.cshtml", etc.
+        }
+    }
+}
---
+++ PRAC 2/Models/MyGroup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PRAC_2.Models
+{
+    public class MyGroup
+    {
+        public string studentNumber {  get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string emailAddress { get; set; }
+        
+        
+
+
+    }
+}
